7fd8a26ecce2dbcd59ad3a3e91e09b777ee4a2f1
luisrnandezc <luisrnandezc@gmail.com>
2025-12-12 11:04:17 -0400

    fix: remove unnecessary brackets in the str method

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -39,5 +39,5 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
-        return f"{{ self.email }} {{ self.role }}"
+        return f"{self.email} {self.role}"
 
